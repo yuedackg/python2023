0dc2d9fd575c9c7fc46a69cdeb8b0ec98f528993
Yoshihiro UEDA <y.ueda@ckg.ac.jp>
2023-07-05 08:58:10 +0900

    fix

---
+++ test/5-3.py
@@ -0,0 +1,9 @@
+lp = 2
+
+str = "hello_"
+
+ret = ''
+
+for mylp in range( lp):
+	ret = ret + str
+print( ret)
---
+++ test/6-2.py
@@ -0,0 +1,10 @@
+for myCount in range( 1, 5, 1):
+	print( "[", myCount, "]", end="")
+	for mySubCount in range( myCount):
+		print( "*", end="")
+	print( "")
+for myCount in range( 5, 0, -1):
+	print( "[", myCount, "]", end="")
+	for mySubCount in range( myCount):
+		print( "*", end="")
+	print( "")
