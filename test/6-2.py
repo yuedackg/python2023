bf1c96f137cc84781b2c8c47f7804d7b553c1f86
Yoshihiro UEDA <y.ueda@ckg.ac.jp>
2023-07-05 17:53:01 +0900

    授業における修正

--- test/6-2.py
+++ test/6-2.py
@@ -1,10 +1,18 @@
-for myCount in range( 1, 5, 1):
-	print( "[", myCount, "]", end="")
-	for mySubCount in range( myCount):
-		print( "*", end="")
+# for myCount in range( 1, 5, 1):
+# 	print( "[", myCount, "]", end="")
+# 	for mySubCount in range( myCount):
+# 		print( "*", end="")
+# 	print( "")
+# for myCount in range( 5, 0, -1):
+# 	print( "[", myCount, "]", end="")
+# 	for mySubCount in range( myCount):
+# 		print( "*", end="")
+# 	print( "")
+
+
+for ko in range( 5):
+	print( "[",ko,"]" , end="")
+	for aaa in range(ko):
+		print( "*" , end="")
 	print( "")
-for myCount in range( 5, 0, -1):
-	print( "[", myCount, "]", end="")
-	for mySubCount in range( myCount):
-		print( "*", end="")
-	print( "")
+	
--- test/6-3.py
+++ test/6-3.py
@@ -1,8 +1,17 @@
 myList = [	[ 1,2,3,4,5,6,7,8],
 			[ 9,10,11,12,13,14,15,16]]
 
+
 # 1行ごとに、16、15、14、・・・と表示するプログラムを作成しなさい
 # フローチャート有
 
 print( myList[1][7])
 print( myList[1][6])
+
+print( myList[0][3]) 
+print( myList[1][0])
+print( myList[0][7])
+print( myList[1][7]) 
+11 は myList[1][2]
+6 
+myList[0][5]
